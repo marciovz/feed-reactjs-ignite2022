00e3cd29b21c9e3e5fe27233357f58a0e7a67b77
marciovz <marciovz@yahoo.com.br>
2022-08-07 16:28:54 -0300

    migração projeto para typescript

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,31 @@
+import { PencilLine } from 'phosphor-react';
+
+import { Avatar } from './Avatar';
+
+import styles from './Sidebar.module.css';
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1632910138458-5bf601f3835e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=40" 
+      />
+
+      <div className={styles.profile}>
+        <Avatar src='https://avatars.githubusercontent.com/u/2254731?v=4'/>
+
+        <strong>Diego Fernandes</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+
+    </aside>
+  )
+}
